040fc26838737943879be1daaf02b590b94f6ed8
Serhii <sergey.melnichuk92@gmail.com>
2021-09-16 19:07:27 +0300

    added new functions for app

--- star-app/src/components/random-planet/random-planet.js
+++ star-app/src/components/random-planet/random-planet.js
@@ -15,17 +15,17 @@ export default class RandomPlanet extends Component {
     error: false
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updatePlanet();
     //this.interval = setInterval(this.updatePlanet, 3000);
-  };
+  }
 
   onPlanetLoaded = (planet) => {
     this.setState({
       planet,
       loading: false,
     });
+    
   };
 
   onError = (error) => {
