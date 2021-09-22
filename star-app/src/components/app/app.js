74ccf9533f1081e8fafee0b1c7de62f8b773a3ce
Serhii <sergey.melnichuk92@gmail.com>
2021-09-22 14:13:37 +0300

    new functions for app

--- star-app/src/components/app/app.js
+++ star-app/src/components/app/app.js
@@ -1,16 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import Header from '../header';
-import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import Header from "../header";
+import RandomPlanet from "../random-planet";
 
-import './app.css';
-import ErrorIndicator from '../error-indicator';
-import SwapiService from '../../services/swapi-services';
+import PeoplePage from "../peple-page/people-page";
+
+import "./app.css";
+import ErrorIndicator from "../error-indicator";
+import SwapiService from "../../services/swapi-services";
 
 export default class App extends Component {
-
   swapiService = new SwapiService();
 
   state = {
@@ -26,44 +25,23 @@ export default class App extends Component {
   };
 
   componentDidCatch() {
-    console.log('Component did Catch');
+    console.log("Component did Catch");
     this.setState({ hasError: true });
-  };
+  }
 
   render() {
-
-    if(this.state.hasError) {
-      return <ErrorIndicator />
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
     }
+
     return (
       <div className="container">
         <Header />
         <RandomPlanet />
-  
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList 
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllPeople}
-            />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson}/>
-          </div>
-        </div>
 
-        <div className="row mb2 mt-2">
-          <div className="col-md-6">
-            <ItemList 
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllPlanets}
-            />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson}/>
-          </div>
-        </div>
+        <PeoplePage />
+
       </div>
     );
-  };
-};
+  }
+}
